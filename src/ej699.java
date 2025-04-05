f979d443ebe2f3b07d38f5673012d9819b21f7c8
OMENPACHOLU\pablo <pablolaneroperez@gmail.com>
2025-04-05 19:07:23 +0200

    El ejercicio resuelto, aunque hay que mirar que se haga con los casos en los que ponga solo una constante, aunque no es muy complicado, es algo que se soluciona con unos pocos ifs, asi que queda un poco de resolver

--- src/ej699.java
+++ src/ej699.java
@@ -17,9 +17,28 @@ public class ej699 {
 
         String ecuacion = sc.nextLine();
         String[] grados = ecuacion.split("x");
+        int coeficiente = 1;
+        int potencia =0;
+
         for (int i = 0; i < grados.length; i++) {
-            System.out.println(grados[i]);
+            if(grados[i].contains("^")){
+                String numero = grados[i].replace('^', '0');
+                String[] numeros = numero.split("[+]");
+                potencia = Integer.valueOf(numeros[0]);
+                boolean potenciaUno = potencia-1 ==1;
+                if(potenciaUno)System.out.printf("%dx+",coeficiente*potencia);
+                else System.out.printf("%dx^%d+",coeficiente*potencia,potencia-1);
+                if(numeros.length==2){
+                    coeficiente = Integer.valueOf(numeros[1]);
+                }else{
+                    coeficiente=1;
+                }
+                
+            }else if(i+1 != grados.length){
+                coeficiente = Integer.parseInt(grados[i]);
+            }
         }
+        System.out.printf("%s%n",coeficiente);
         
         
     }
